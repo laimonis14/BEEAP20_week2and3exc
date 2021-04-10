0baf50f9555cef7beb9fa534bbce49bd050e9504
unknown <laimonis20100@student.hamk.fi>
2021-04-10 10:24:11 +0300

    title changed

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from matplotlib.figure import Figure
 class App:
     def __init__(self, root):
         # setting title
-        root.title("undefined")
+        root.title("DATA Browser")
         # setting window size
         width = 600
         height = 500
